b4fce2b2280bc5f3cfa1c95c28c82b36a38574df
Lera-admin <lerasimonova54@mail.ru>
2017-05-07 20:08:08 +0700

    Ready code

--- lib.h
+++
@@ -1,7 +0,0 @@
-#include <stdbool.h> 
-
-void menu();
-void vvod();
-void file();
-void sort(bool end);
-void sort1(bool end);
--- main.c
+++
@@ -1,6 +0,0 @@
-#include "lib.h"
-#include <stdio.h>
-int main(){
-    menu();
-    return 0;
-}
