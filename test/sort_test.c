334066bfe9e57ee6ac1206c9353d76a09fe62ed0
Meleriss <berezina.mania@mail.ru>
2017-05-30 03:01:15 +0700

    Some test

--- test/sort_test.c
+++ test/sort_test.c
@@ -13,3 +13,33 @@ CTEST(sort, sort_1)
 
 	ASSERT_STR(str, real);
 }
+
+CTEST(sort, sort_2)
+{
+	char real[] = "1 2 3";
+	char test[] = "2 3 1";
+
+	char *str = separation(test, strlen(test));
+
+	ASSERT_STR(str, real);
+}
+
+CTEST(sort, sort_3)
+{
+	char real[] = "ab aa ca";
+	char test[] = "ca ab aa";
+
+	char *str = separation(test, strlen(test));
+
+	ASSERT_STR(str, real);
+}
+
+CTEST(sort, sort_4)
+{
+	char real[] = "21 7 aaa bs";
+	char test[] = "bs aaa 7 21";
+
+	char *str = separation(test, strlen(test));
+
+	ASSERT_STR(str, real);
+}
