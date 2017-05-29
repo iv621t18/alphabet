765add5bf6036b0249f2ca9ffb27b844a1150e27
Meleriss <berezina.mania@mail.ru>
2017-05-30 02:30:30 +0700

    sort.c 2.0

--- src/sort.c
+++ src/sort.c
@@ -6,9 +6,9 @@ void in_file(char *mass1)
 {
     FILE* itog;
     itog = fopen("itog.txt", "w");
-    fwrite(&mass1, sizeof(mass1), 1, itog);
+    fprintf(itog, "%s", mass1);
     fclose(itog);
-    printf("\nResult:\n%s", mass1);
+    printf("\nResult: %s", mass1);
     printf("\n");
 }
 
@@ -20,60 +20,66 @@ int sort1(int var, char *word1, char *word2, char *rez, int *kl, int *p2, int *p
         *kl = 1;
         if(var == 1)
             strcat(rez, word1);
-    } else {
-        strcat(rez, word1);
-        strcat(rez, " ");
+        } else {
+            strcat(rez, word1);
+            strcat(rez, " ");
+            if(var == 1) {
+                strcat(rez, word2);
+            }
+            else {
+                word1[0] = '\0';
+                strcat(word1, word2);
+            }
+        }
         if(var == 1)
-            strcat(rez, word2);
-        else
-            memcpy(word1, word2, 255);
-    }
-        if(var == 1)
-            memset(word1, (char)0, l);
-        memset(word2, (char)0, l);
+            word1[0] = '\0';
+            word2[0] = '\0';
         if(var == 1) {
             *p2 = 0;
             *p1 = 1;
         }
-        else 
-            *p2 = 1;
+    else 
+    *p2 = 1;
     return 0;
 }
 
-int separation(char *mass1, int l) 
+char *separation(char *mass1, int l) 
 {
     int kl = 0, p1 = 1, p2 = 0; //ключи
     int y = 1; //флаг конца файла
     int z = 0; //флаг не конца файла
     char rez[255], word1[255], word2[255];
+    for (int i = 0; i < 255; i++) {
+        rez[i] = 0;
+        word1[i] = 0;
+        word2[i] = 0;
+    }
     do {
- 	    kl = 0;
+     	kl = 0;
         for(int n = 0; n < l; n++) {
             if(p1 == 1 && mass1[n] != ' ') {
-                strncat(word1, &mass1[n], 1);
-            }
-            else if(p1 == 1 && word1[0] != 0 && mass1[n+1] != ' ' && mass1[n] == ' ') {
-                p2 = 1;
-                p1 = 0;
-                continue;
-            }
-            else if(p2 == 1 && mass1[n] != ' ') {
-                strncat(word2, &mass1[n], 1);
-            }
-            else if(mass1[n] == ' ' && mass1[n+1] != ' ' && p2 == 1) {
-                p2 = 0;
-            }
-            if((p2 == 0 && p1 == 0) || (n == l-1)) {
-                if(n == l-1)
-                    sort1(y, word1, word2, rez, &kl, &p2, &p1, l);
-                else sort1(z, word1, word2, rez, &kl, &p2, &p1, l);
+            strncat(word1, &mass1[n], 1);
+        } else if (p1 == 1 && word1[0] != 0 && mass1[n+1] != ' ' && mass1[n] == ' ') {
+            p2 = 1;
+            p1 = 0;
+            continue;
+        } else if (p2 == 1 && mass1[n] != ' ') {
+            strncat(word2, &mass1[n], 1);
+        } else if (mass1[n] == ' ' && mass1[n+1] != ' ' && p2 == 1) {
+            p2 = 0;
+        }
+        if((p2 == 0 && p1 == 0) || (n == l-1)) {
+            if(n == l-1) {
+                sort1(y, word1, word2, rez, &kl, &p2, &p1, l);
+            } else {
+                sort1(z, word1, word2, rez, &kl, &p2, &p1, l);
             }
         }
-    memcpy(mass1, rez, 255);
-    memset(rez, (char)0, sizeof(rez));
+        }
+        mass1[0] = '\0';
+        strcat(mass1, rez);
+        rez[0] = '\0';
     } while(kl != 0);
-
-    in_file(mass1);
-    return 0;
+    return mass1;
 }
 
