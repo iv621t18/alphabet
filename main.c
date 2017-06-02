07a03dfdf147105b1f151075bb640a6b10881f74
Meleriss <berezina.mania@mail.ru>
2017-06-03 01:21:30 +0700

    Memory for an array of pointers

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <sys/types.h>//Консанты
 #include <sys/stat.h>//Прототип chmod
 #include <unistd.h>//Прототип access
+#include <stdlib.h>
+
+#define MAX_NUM_STR 512
 
 int main (int argc, char* argv[])
 {
@@ -25,6 +28,12 @@ int main (int argc, char* argv[])
 	FILE *in = fopen(argv[1], "r");
 	if (in == NULL) {
 		printf("Usage: alphabet <input_file>\n");
+		return 1;
+	}
+
+	char **str = calloc(MAX_NUM_STR, sizeof(char*));
+	if (str == NULL) {
+		printf("SIGSEGV\n");
 		return 1;
 	}
 
