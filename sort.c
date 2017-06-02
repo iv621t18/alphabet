c3ba7f3de44a12bfddcdccf5fbf058eff052b581
Lera-admin <lerasimonova54@mail.ru>
2017-06-03 02:56:05 +0700

    Edit sort.c

--- sort.c
+++ sort.c
@@ -1,20 +1,17 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include<string.h>
+#include <string.h>
+#include "sort.h"
 
-char abc[]={'c','b','a','\0'};
-
-static int
-comp(const void *word1, const void *word2){
-
-	return strcmp(word1, word2);
-
+static int cmpstringp(const void *p1, const void *p2)
+{
+   return strcmp(*(char* const*)p1, *(char* const*)p2);
 }
-int main(){
 
-	qsort(&abc,4,sizeof(char*),comp);
-	for (int j = 0; j < 4; j++)
-		printf("%d\n",abc[j]);
-
-	return 0;
+char **sort(char **str, int count)
+{
+	if (str == NULL) {
+		return NULL;
+	}
+	qsort(str, count, sizeof(char*), cmpstringp);
+	return str;
 }
