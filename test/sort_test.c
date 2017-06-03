5ea3d17591c5a6efeb28009b8b9b1b6c6e2f9649
Антипова Екатерина Петровна <iv621s01@mwp12.csc.local>
2017-06-03 12:59:53 +0700

    Test4 -  digital

--- test/sort_test.c
+++ test/sort_test.c
@@ -30,3 +30,16 @@ CTEST(SORT_TEST, ARRAY_IS_NULL)
 
 	ASSERT_NULL(exp);
 }
+
+CTEST(SORT_TEST, DIGITAL_TEST)
+{
+	char *exp[] = {"10", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0"};
+
+	sort(exp, 11);
+
+	char *real[] = {"0", "1", "10", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+	for (int i = 0; i < 11; i++) {
+		ASSERT_STR(exp[i], real[i]);
+	}
+}
